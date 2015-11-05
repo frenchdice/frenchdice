8439f267e328d13753130af272fb300b3db523a2
frenchdice <coco65400@gmail.com>
2015-11-05 16:13:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var el = React.DOM;
 // Generates UUID for uniquely tagging components
 var genUuid = function() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = 10|0, v = c == 'x' ? r : (r&0x3|0x8);
+    var r = Math.random()|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
   });
 };
